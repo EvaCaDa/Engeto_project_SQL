b465fc947a514296c0d78f9ee983df69def9e36f
Eva Cajzlová <dankova.eva01@gmail.com>
2023-11-01 18:21:34 +0100

    Creating of primary table - continue

--- Projekt_ECD.sql
+++ Projekt_ECD.sql
@@ -76,47 +76,46 @@ FROM czechia_payroll cp
 WHERE
 	value_type_code = 5958 -- prumerna hruba mzda zamestnance
 	AND calculation_code = 100; -- fyzicky plat (kod 200 je prepocteny na plne uvazky) - lepe bude ilustrovat realnou dostupnost potravin, o kterou nam jde
--- kdyz je NULL u industry_branch_code, tak je to prumer pro vsechny branche? Neni, proste nevim, co je to za cislo.
 
+CREATE OR REPLACE VIEW v_eva_cajzlova_payroll_ibch_null AS
+	SELECT *
+	FROM czechia_payroll cp
+	WHERE
+		value_type_code = 5958
+		AND calculation_code = 100
+		AND industry_branch_code IS NULL;
+-- NULL u industry branch code - jedna hodnota ke kazdemu ctvrtleti kazdeho roku, je to prumer?
 
-SELECT *
-FROM czechia_payroll cp
-WHERE
-	value_type_code = 5958 -- prumerna hruba mzda zamestnance
-	AND calculation_code = 100
-	AND payroll_year = 2000
-	AND payroll_quarter = 1; -- fyzicky plat (kod 200 je prepocteny na plne uvazky) - lepe bude ilustrovat realnou dostupnost potravin, o kterou nam jde
--- kdyz je NULL u industry_branch_code, tak je to prumer pro vsechny branche? Neni. WTF? podle toho ciselniku z csu a dokumentace tam uplne chybi dve kategorie (t a u), fakt nevim, co to je za cislo	
-	
+CREATE OR REPLACE VIEW v_eva_cajzlova_payroll_ibch_not_null_avg AS		
+	SELECT
+		payroll_year,
+		payroll_quarter,
+		avg(value) AS average_value_ibch_not_null
+	FROM czechia_payroll cp
+	WHERE
+		value_type_code = 5958 -- prumerna hruba mzda zamestnance
+		AND calculation_code = 100
+		AND industry_branch_code IS NOT NULL
+	GROUP BY
+		payroll_year,
+		payroll_quarter
+	ORDER BY
+		payroll_year,
+		payroll_quarter;
+
 SELECT
-	*
-FROM czechia_payroll cp
-WHERE
-	calculation_code = 100
-	AND payroll_year = 2000
-	AND payroll_quarter = 1
-	AND industry_branch_code IS NOT NULL; -- 11593
-
-
-SELECT 1720/20
-
-SELECT avg(value)
-FROM czechia_payroll cp
-WHERE
-	value_type_code = 5958
-	AND calculation_code = 100;
-
-SELECT avg(value)
-FROM czechia_payroll cp
-WHERE
-	value_type_code = 5958
-	AND calculation_code = 200;
-
+	pinna.*,
+	pin.value AS value_ibch_null
+FROM v_eva_cajzlova_payroll_ibch_not_null_avg pinna
+JOIN v_eva_cajzlova_payroll_ibch_null pin
+	ON pinna.payroll_year = pin.payroll_year
+	AND pinna.payroll_quarter = pin.payroll_quarter;
+-- NULL ibc neni prumer, nevim, co by to mohlo byt, odignoruju to v analyze
 
 CREATE OR REPLACE TABLE t_eva_cajzlova_czechia_payroll_final AS 
 	SELECT
-		cp.id,
-		cp.value,
+		cp.id AS payroll_id,
+		cp.value AS payroll_value,
 		cpu.name AS payroll_value_name,
 		cp.industry_branch_code,
 		cpib.name AS branch_code_name,
@@ -129,29 +128,21 @@ CREATE OR REPLACE TABLE t_eva_cajzlova_czechia_payroll_final AS
 		ON cp.unit_code = cpu.code
 	WHERE
 		cp.value_type_code = 5958 -- prumerna hruba mzda zamestnance
-		AND cp.calculation_code = 100; -- fyzicky plat
+		AND cp.calculation_code = 100  -- fyzicky plat
+		AND industry_branch_code IS NOT NULL -- ignoruji ty prazdne hodnoty, nevim, co je TO za cislo, tak co bych s tim delala?
+	ORDER BY
+		cp.industry_branch_code,
+		cp.payroll_year,
+		cp.payroll_quarter;
 
-
-SELECT
-	min(payroll_year), -- 2000
-	max(payroll_year) -- 2021
-FROM czechia_payroll cp
-WHERE
-	value_type_code = 5958
-	AND calculation_code = 100; 
 
 -- Priprava primarni tabulky - ceny
 
 SELECT *
 FROM czechia_price cp;
 -- limitace, pro ceny to mam rozbite na kraje, ale pro mzdy ne --- mam se zamerit jen na cr, nebo resit i kraje, kdyz pro to nemuzu udelat totez ve mzdach?
-
-SELECT
-	min(YEAR(date_from)), -- 2006
-	max(YEAR(date_from))-- 2018
-FROM czechia_price cp;
-
--- Zacneme omezenim na roky.
+-- NULL u kraje znamena, ze jsou to data jen pro celou CR, staci mi jenom ta?
+-- zkusim resit jen pro celou cr, kdyz nemam protipol u mezd, tak by to stejne bylo divne; MOZNA PRIDAT?
 
 SELECT
 	*,
@@ -161,15 +152,13 @@ FROM czechia_price cp
 WHERE
 	year(date_from) != year(date_to); -- good, vsude sedi rok, cajk
 
-	
 SELECT
 	*,
 	quarter(date_from),
 	quarter(date_to)
 FROM czechia_price cp
 WHERE
-	quarter(date_from) != quarter(date_to) -- ok, mam problem, co s tim
-ORDER BY quarter(date_to);
+	quarter(date_from) != quarter(date_to); -- ok, mam problem, co s tim
 
 SELECT
 	*,
@@ -182,54 +171,42 @@ WHERE
 	quarter(date_from) != quarter(date_to);
 
 SELECT
+	year(date_from),
 	quarter(date_from),
 	min(dayofmonth(date_to)),
 	max(dayofmonth(date_to))
 FROM czechia_price cp
 WHERE
 	quarter(date_from) != quarter(date_to)
-GROUP BY quarter(date_from);
-
-SELECT
-	*,
-	dayofmonth(date_from),
-	dayofmonth(date_to),
-	quarter(date_from),
-	quarter(date_to),
-	dayname(date_from)
-FROM czechia_price cp
-WHERE
-	quarter(date_from) = 1 
-	AND quarter(date_to) = 2
-	AND (year(date_from) BETWEEN 2006 AND 2018);
--- tyden ma sedm dni, pokud bude dayofmonth(date_to) 4 a vice, tak to prsknu do vetsiho quarteru
+GROUP BY
+	year(date_from),
+	quarter(date_from);
 
 SELECT
 	*,
 	year(date_from) AS price_year,
 	CASE
 		WHEN quarter(date_from) = quarter(date_to) THEN quarter(date_from)
-		WHEN quarter(date_from) != quarter(date_to) AND dayofmonth(date_to) < 4 THEN quarter(date_from)
+		WHEN quarter(date_from) != quarter(date_to) AND dayofmonth(date_to) < 4 THEN quarter(date_from) -- tyden ma sedm dni, pokud bude dayofmonth(date_to) 4 a vice, tak to prsknu do vetsiho quarteru
 		ELSE quarter(date_to) 
 	END AS price_quarter
 FROM czechia_price cp;
 
 SELECT
-	*,
 	year(date_from) AS price_year,
 	CASE
 		WHEN quarter(date_from) = quarter(date_to) THEN quarter(date_from)
 		WHEN quarter(date_from) != quarter(date_to) AND dayofmonth(date_to) < 4 THEN quarter(date_from)
 		ELSE quarter(date_to) 
-	END AS price_quarter
+	END AS price_quarter,
+	quarter(date_from),
+	dayofmonth(date_to)
 FROM czechia_price cp
 WHERE 
 	quarter(date_from) != quarter(date_to)
 GROUP BY
 	price_year,
 	price_quarter;
-
--- t_{jmeno}_{prijmeni}_project_SQL_primary_final
 
 CREATE OR REPLACE TABLE t_eva_cajzlova_czechia_price_year_quarter AS
 	SELECT
@@ -245,24 +222,38 @@ CREATE OR REPLACE TABLE t_eva_cajzlova_czechia_price_year_quarter AS
 		END AS price_quarter
 	FROM czechia_price cp;
 
-SELECT
-	*
-FROM t_eva_cajzlova_czechia_price_year_quarter cpyq
-WHERE
-	category_code IS NULL;
-
 CREATE OR REPLACE TABLE t_eva_cajzlova_czechia_price_final AS
 	SELECT
-		cpyq.id,
-		cpyq.value,
+		cpyq.id AS price_id,
+		cpyq.value AS price_value,
 		cpyq.category_code,
-		cpc.name,
+		cpc.name AS category_name,
 		concat(cpc.price_value, ' ', cpc.price_unit) AS price_value_unit,
-		cpyq.region_code,
 		cpyq.price_year,
 		cpyq.price_quarter
 	FROM t_eva_cajzlova_czechia_price_year_quarter cpyq
 	JOIN czechia_price_category cpc
-		ON cpyq.category_code = cpc.code;
+		ON cpyq.category_code = cpc.code
+	WHERE
+		region_code IS NULL -- vybiram jen prumer pro celou CR
+	ORDER BY
+		cpyq.category_code,
+		cpyq.price_year,
+		cpyq.price_quarter;
+
+
+-- Spojeni predpripravenych payroll a price tabulek
+SELECT
+	min(payroll_year), -- 2000
+	max(payroll_year), -- 2021
+FROM t_eva_cajzlova_czechia_payroll_final teccpf;
+
+SELECT
+	min(price_year), -- 2006
+	max(price_year)-- 2018
+FROM t_eva_cajzlova_czechia_price_final teccpf;
+
+-- 2006 - 2018
+-- t_{jmeno}_{prijmeni}_project_SQL_primary_final
 
 

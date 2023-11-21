28152060447174ecf9ec34b7f7d997b6cce0a1c6
Eva Cajzlová <dankova.eva01@gmail.com>
2023-11-21 18:56:35 +0100

    Question 1

---
+++ Question1_script.sql
@@ -0,0 +1,50 @@
+/* 
+ * OTÁZKA 1
+ * Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
+ * 
+ * Zdrojová data:
+ * t_eva_cajzlova_project_SQL_primary_final
+ */
+
+CREATE VIEW v_eva_cajzlova_question1_avg_payroll_year AS
+	SELECT
+		industry_branch_code,
+		branch_code_name,
+		payroll_year,
+		round(avg(payroll_value), 2) AS average_payroll
+	FROM t_eva_cajzlova_project_SQL_primary_final
+	GROUP BY
+		industry_branch_code,
+		payroll_year
+	ORDER BY
+		industry_branch_code,
+		payroll_year;
+
+CREATE TABLE t_eva_cajzlova_project_sql_question1_final AS
+	SELECT
+		tab1.*,
+		tab2.average_payroll AS last_year_average_payroll,
+		tab1.average_payroll - tab2.average_payroll AS interannual_difference,
+		CASE
+			WHEN tab1.average_payroll > tab2.average_payroll THEN 1
+			ELSE 0
+		END AS average_payroll_growth
+	FROM v_eva_cajzlova_question1_avg_payroll_year tab1
+	JOIN v_eva_cajzlova_question1_avg_payroll_year tab2
+		ON tab1.payroll_year = tab2.payroll_year + 1
+		AND tab1.industry_branch_code = tab2.industry_branch_code;
+
+CREATE TABLE t_eva_cajzlova_project_sql_question1_payroll_decrease AS
+	SELECT *
+	FROM t_eva_cajzlova_project_sql_question1_final
+	WHERE average_payroll_growth = 0;
+
+
+SELECT *
+FROM t_eva_cajzlova_project_sql_question1_final
+ORDER BY interannual_difference DESC;
+
+SELECT
+	round(avg(interannual_difference), 2) AS average_interannual_difference
+FROM t_eva_cajzlova_project_sql_question1_final
+ORDER BY interannual_difference DESC;
--- README.md
+++ README.md
@@ -5,3 +5,11 @@ First Engeto project
 
 Primární tabulka obsahuje data průměrných hrubých mezd a průměrných cen potravin v České republice za období 2006-2018.
 Vzhledem k tomu, že hlavním cílem výzkumu je analýza dostupnosti základních potravin široké veřejnosti, průměrné hrubé mzdy odpovídají fyzickému platu zaměstnanců (nejsou přepočítány na plné úvazky). Je tak lépe vidět, co si lidé skutečně mohli dovolit v daných letech koupit.
+
+# Otázka 1: Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
+
+Na základě dostupných dat lze říci, že mezi lety 2006-2018 měly průměrné mzdy tendenci spíše růst než klesat. Průměrný meziroční nárůst mezd ve všech analyzovaných oblastech byl 970 Kč.
+Největší meziroční nárůst průměrných mezd nastal v oblasti "Výroba a rozvod elektřiny, plynu, tepla a klimatiz. vzduchu", kde si zaměstnanci v roce 2008 oproti předchozímu roku polepšili o 4090 Kč.
+Navzdory patrné rostoucí tendenci se většina oblastí (14 z 19) setkala s alespoň jedním meziročním poklesem průměrných mezd. Pokles se vyhnul oblastem "Zpracovatelský průmysl", "Doprava a skladování", "Administrativní a podpůrné činnosti", "Zdravotní a sociální péče" a "Ostatní činnosti".
+Nejčastěji klesaly průměrné mzdy v oblasti "Těžba a dobývání", a to mezi lety 2008-2009, 2012-2014 a 2015-2016.
+Největší meziroční pokles mezd nastal v oblasti "Peněžnictví a pojišťovnictví", kde si zaměstnanci v roce 2013 meziročně pohoršili o 4530 Kč.
